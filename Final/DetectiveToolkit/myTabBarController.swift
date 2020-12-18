a8ada511bb66320a0dc5624a3541eaa5364532eb
Jonathan Titpon <jonathantipton@u.boisestate.edu>
2020-12-18 02:42:10 -0700

    Final Project
    Final Project. Added ARScene for Disguise tab

---
+++ Final/DetectiveToolkit/myTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  myTabBarController.swift
+//  DetectiveToolkit
+//
+//  Created by Jonathan Tipton on 9/9/20.
+//  Copyright © 2020 Jonathan Tipton. All rights reserved.
+//
+
+import UIKit
+
+class myTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 2
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
